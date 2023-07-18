3206c1133c4df3a7d4914ff8a9b29035ba4cb3f6
smidr <vromabz@gmail.com>
2023-07-18 12:02:24 +0300

    add a table

--- main.py
+++ main.py
@@ -6,13 +6,39 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", format="A4", unit="mm")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_num = filename.split("-")[0]
-    deal_date = filename.split("-")[1]
+    invoice_num, date = filename.split("-")
+
     pdf.set_font(family="Times", size=20, style="B")
     pdf.cell(w=50, h=8, txt=f"Invoice #{invoice_num}", ln=1)
-    pdf.cell(w=50, h=8, txt=f"Date {deal_date}")
+
+    pdf.set_font(family="Times", size=20, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+
+    # add a header
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1, align="L")
+    pdf.cell(w=60, h=8, txt=columns[1], border=1, align="L")
+    pdf.cell(w=40, h=8, txt=columns[2], border=1, align="L")
+    pdf.cell(w=30, h=8, txt=columns[3], border=1, align="L")
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, align="L", ln=1)
+
+    # add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=60, h=8, txt=str(row['product_name']), border=1)
+        pdf.cell(w=40, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
     pdf.output(f"PDF/{filename}.pdf")
